2a30a4eb839536fe31ce7f55c8f49b949ddf5a7e
ubikTeacher <y_takagishi@ubik.ac.jp>
2025-06-27 08:54:44 +0900

    c

---
+++ Assets/Scripts/TitleGameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TitleGameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayerPrefs.SetInt("ClearStage", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
